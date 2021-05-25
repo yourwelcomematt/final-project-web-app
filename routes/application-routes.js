066872000ea8c8c178ac0824586d2903745f7708
Declan Williams <dwil845@aucklanduni.ac.nz>
2021-05-25 03:46:49 +0000

    New acc view

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,10 +5,16 @@ const testDao = require("../modules/test-dao.js");
 
 router.get("/", async function(req, res) {
 
-    res.locals.title = "My route title!";
-    res.locals.allTestData = await testDao.retrieveAllTestData();
+    // res.locals.title = "My route title!";
+    // res.locals.allTestData = await testDao.retrieveAllTestData();
 
     res.render("home");
+});
+
+router.get("/newaccount", async function(req, res) {
+
+
+    res.render("newaccount");
 });
 
 module.exports = router;
