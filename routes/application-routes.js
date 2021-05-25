9c5bbdda0d86be086d79bb84a3cd49be7a3f000d
mattding <mdin874@aucklanduni.ac.nz>
2021-05-25 14:29:45 +1200

    Created basic layout and CSS for website

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,8 +5,8 @@ const testDao = require("../modules/test-dao.js");
 
 router.get("/", async function(req, res) {
 
-    res.locals.title = "My route title!";
-    res.locals.allTestData = await testDao.retrieveAllTestData();
+    // res.locals.title = "My route title!";
+    // res.locals.allTestData = await testDao.retrieveAllTestData();
 
     res.render("home");
 });
