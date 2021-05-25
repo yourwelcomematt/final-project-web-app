805bac50a6156fc17508442dc2cfa92c56cd16d7
FrutalBellow <dwil845@aucklanduni.ac.nz>
2021-05-25 16:44:55 +1200

    Edited to fix merge conflicts

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,18 +5,11 @@ const testDao = require("../modules/test-dao.js");
 
 router.get("/", async function(req, res) {
 
-    // res.locals.title = "My route title!";
+    // res.locals.title = “My route title!“;
     // res.locals.allTestData = await testDao.retrieveAllTestData();
 
     res.render("home");
 });
-
-router.get("/newaccount", async function(req, res) {
-
-    res.render("newaccount");
-    
-});
-
 
 router.get("/login", async function(req, res) {
     res.render("login");
@@ -28,7 +21,7 @@ router.post("/login", async function(req, res) {
 
     console.log(username);
     console.log(password);
-    
+
     res.redirect("/");
 });
 
