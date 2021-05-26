9abedcee7bb66f42cba85244dd26717db4688bca
Hollie White <hwhi660@aucklanduni.ac.nz>
2021-05-27 10:53:20 +1200

    Working on wysiwyg

--- public/wysiwyg.js
+++ public/wysiwyg.js
@@ -3,6 +3,15 @@ tinymce.init({
     menubar: false,
     plugins: 'autoresize lists save emoticons wordcount',
     autoresize_bottom_margin: 50,
-    toolbar: 'undo redo | styleselect | forecolor | bold italic underline | numlist bullist | emoticons | wordcount | save',
+    toolbar: 'undo redo | styleselect | forecolor | bold italic underline | numlist bullist | emoticons | wordcount',
     images_file_types: 'jpeg,jpg,png,gif'
     });
+
+function getNewArticleContent() {
+    const newArticleContent = tinymce.get("newArticle").getContent();
+    console.log(newArticleContent);
+}
+
+function updateArticleContent(newArticleContent) {
+    appendChild
+}
