f632cb1ab5cf1d4ddd66a7ce3b64de6d265ff84f
mattding <mdin874@aucklanduni.ac.nz>
2021-06-02 18:26:56 +1200

    Renamed result variable to passwordsMatch in auth-routes.js

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -28,11 +28,11 @@ router.post("/login", async function(req, res) {
     // console.log(hash);
 
     if (hash != undefined) {
-        const result = await bcrypt.compare(plaintextPassword, hash.password);
-        // console.log(result);
+        const passwordsMatch = await bcrypt.compare(plaintextPassword, hash.password);
+        // console.log(passwordsMatch);
 
          // if the passwords match...
-        if (result) {
+        if (passwordsMatch) {
 
             const user = await authDao.retrieveUserWithCredentials(username, hash.password);
 
