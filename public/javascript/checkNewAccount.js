b410ce5eb1fd865a70538fa02b87357ab2d3235e
mattding <mdin874@aucklanduni.ac.nz>
2021-06-04 18:10:07 +1200

    Added comments to checkNewAccount.js

--- public/javascript/checkNewAccount.js
+++ public/javascript/checkNewAccount.js
@@ -1,3 +1,7 @@
+/** On window load, disable the submit button, and only enable it
+ * once the user has entered both a valid username (i.e. not already
+ * taken) and matching passwords
+ */
 window.addEventListener("load", function () {
 
     const usernameInput = document.querySelector("#txtUsername");
@@ -8,12 +12,14 @@ window.addEventListener("load", function () {
     const usernameMessage = document.querySelector("#usernameMessage");
     const confirmPasswordMessage = document.querySelector("#confirmPasswordMessage");
 
+    // Disables the submit button
     submitButton.disabled = true;
 
     let validUsername;
     let validPassword;
 
 
+    // Checks if the username is valid each time the user inputs a username
     usernameInput.addEventListener("change", async function() {
         usernameMessage.innerHTML = "";
         validUsername = true;
@@ -38,12 +44,18 @@ window.addEventListener("load", function () {
     confirmPasswordInput.addEventListener("change", checkPasswords);
 
 
+    // Retrieves all usernames from the database
     async function getUsernames() {
         const response = await fetch(`./usernames`);
         const reponseObject = await response.json();
         return reponseObject;
     };
     
+    /** 
+     * Checks if the passwords entered in the two password fields
+     * are matching, and enables the submit button if they do. If not,
+     * the submit button remains disabled and a warning message is displayed.
+     */
     function checkPasswords() {
         confirmPasswordMessage.innerHTML = "";
         validPassword = true;
