10a5b2833ac746f9435e7433a72564c9f189a805
mattding <mdin874@aucklanduni.ac.nz>
2021-06-04 17:57:42 +1200

    Added comment to database.js

--- modules/database.js
+++ modules/database.js
@@ -1,3 +1,7 @@
+/**
+ * Begins the process of opening the database file and returns a Promise,
+ * which is exported and can be used to run SQL
+ */
 const sqlite = require("sqlite");
 const dbPromise = sqlite.open("./project-database.db");
 module.exports = dbPromise;
