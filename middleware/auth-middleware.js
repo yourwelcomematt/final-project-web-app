5b38c1e80ed95ce4b9cb19c63f1129358fe9a135
mattding <mdin874@aucklanduni.ac.nz>
2021-06-04 17:18:35 +1200

    Added comments to auth-middleware.js

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,19 +1,34 @@
 const authDao = require("../modules/auth-dao.js");
 
+
+/**
+ * Sets res.locals.user to the logged in user using their authentication token cookie,
+ * or sets it to undefined if no authentication cookie exists (i.e the user is not logged in).
+ * The next function is then called.
+ */
 async function addUserToLocals(req, res, next) {
+
     const user = await authDao.retrieveUserWithAuthToken(req.cookies.authToken);
     res.locals.user = user;
     next();
 }
 
+
+/**
+ * Verifies if the user is logged in -
+ * if a user exists in res.locals.user, call the next function,
+ * otherwise redirect back to the login page.
+ */
 function verifyAuthenticated(req, res, next) {
+
     if (res.locals.user) {
         next();
     }
     else {
         res.redirect("./login");
-    }
+    } 
 }
+
 
 module.exports = {
     addUserToLocals,
