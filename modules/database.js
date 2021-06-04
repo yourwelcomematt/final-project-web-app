6322e78803f7457747adb9ce70d2520bc8156e75
mattding <mdin874@aucklanduni.ac.nz>
2021-06-04 17:59:29 +1200

    Reformatted comments in database.js

--- modules/database.js
+++ modules/database.js
@@ -1,7 +1,8 @@
-/**
- * Begins the process of opening the database file and returns a Promise,
- * which is exported and can be used to run SQL
- */
+// Imports the sqlite package
 const sqlite = require("sqlite");
+
+// Begins the process of opening the database file and returns a Promise
 const dbPromise = sqlite.open("./project-database.db");
+
+// Exports the promise, which can be used to run SQL
 module.exports = dbPromise;
